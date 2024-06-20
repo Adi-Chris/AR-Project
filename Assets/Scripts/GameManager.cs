e74f430d9fc22a8fcade99517b9b40df037251d5
Seryxss <118601385+Seryxss@users.noreply.github.com>
2024-06-20 19:40:47 +0700

    Finish (Tolong cek in), Pause sudah

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
-    //[SerializeField] private TMP_Text testingGyroText;
+    [SerializeField] private GameObject playGameCanvas;
+    [SerializeField] private GameObject maze;
+    [SerializeField] private PC_Gyro playerController;
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private GameObject namaMaze;
+    //[SerializeField] private GameObject buttonCanvas;
+    //[SerializeField] private GameObject arrowCanvas;
+    //[SerializeField] private GameObject spikeCanvas;
+    //[SerializeField] private GameObject threeDCanvas;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject finishTrigger;
+    public GameObject gameFinishCanvas;
+
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameFinishCanvas.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TestDebug() {
+        SceneManager.LoadScene(1);
+    }
+    public void EnterMazePortal()
     {
+        SceneManager.LoadScene(1);
+    }
+    public void EnterMazeButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void EnterMazeArrow()
+    {
+       SceneManager.LoadScene(3);
+    }
+    public void EnterMazeSpike()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void EnterMaze3D()
+    {
+        SceneManager.LoadScene(5);
+    }
+     
+    public void PlayGame()
+    {
+        namaMaze.SetActive(false);
+        playGameCanvas.SetActive(false);
+        pauseButton.SetActive(true);
+        maze.SetActive(true);
+        if (playerController != null) {
+            playerController.InitializeOrientation();
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        namaMaze.SetActive(true );
+        maze.SetActive(false);
+        pauseCanvas.SetActive(true);
+        pauseButton.SetActive(false );
+    }
 
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        namaMaze.SetActive(false);
+        maze.SetActive(true);
+        pauseCanvas.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //public void ScannedMazeButton()
+    //{
+    //    buttonCanvas.SetActive(true);
+    //}
+    //public void ScannedMazeArrow()
+    //{
+    //    arrowCanvas.SetActive(true);
+    //}
+    //public void ScannedMazeSpike()
+    //{
+    //    spikeCanvas.SetActive(true);
+    //}
+    //public void ScannedMaze3D()
+    //{
+    //    threeDCanvas.SetActive(true);
+    //}
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
     }
 }
